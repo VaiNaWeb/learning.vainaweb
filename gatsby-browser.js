54afd3cc4defc13263243e66949e68f1dd55ad4e
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2021-06-08 21:04:58 -0300

    First Try on Fix Reload Page Layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+import ReactDOM from 'react-dom'
 
-// You can delete this file if you're not using it
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
